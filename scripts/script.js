6ed578e7f223d1df43dc6b0ae57f56efa4b38d26
Horst Fellenberg <horst.fellenberg@tuwien.ac.at>
2021-03-21 14:23:23 +0100

    now up to 4 faces can be labeled

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const Reactive = require('Reactive');
 const TouchGestures = require('TouchGestures');
 const Scene = require('Scene');
 const Diagnostics = require('Diagnostics')
-const FaceTracking = require('FaceTracking')
+const FaceTracking = require('FaceTracking');
 
 const names = ['creep', 'stalker', 'loner', 'weirdo', 'sad', 'social media addict', 'insecure', 'jealous', 'self sabotaging', 'anger problem', 'anxious', 'narcissist', 'attention seeker', 'inhibited', 'cautious', 'skeptic', 'not ok', 'distant', 'misanthrope', 'naive', 'disillusioned',
 'hopeless'];
@@ -16,22 +16,19 @@ async function newDisp(label) {
     ", " + confidence);
 }
 
-
-//map objects and make predictions for face
-async function mapFace(face) {
-  const canvas = await Scene.root.findFirst('face1');
-  const label = await Scene.root.findFirst('label1');
-  const confidence = await Scene.root.findFirst('confidence1')
+async function mapFace(i) {
+  let face = FaceTracking.face(i);
+  let canvas = await Scene.root.findFirst('face_'+i);
+  Diagnostics.log(canvas.name)
+  let label = await Scene.root.findFirst('label_'+i);
 
   //map face coordinates to canvas
-  canvas.transform.x = face.cameraTransform.x
-  canvas.transform.y = face.cameraTransform.y
-  canvas.transform.z = face.cameraTransform.z
-  canvas.visible = face.isTracked
-
+  canvas.transform.x = face.cameraTransform.x;
+  canvas.transform.y = face.cameraTransform.y;
+  canvas.transform.z = face.cameraTransform.z;
+  canvas.hidden = Reactive.not(face.isTracked);
 
   //make new prediction when face visibility changes
-  
   face.isTracked.monitor().subscribe(function() {
     newDisp(label);
   });
@@ -45,5 +42,9 @@ async function mapFace(face) {
   newDisp(label);
 };
 
-//map first face
-mapFace(FaceTracking.face(0));
+//map faces
+mapFace(0);
+mapFace(1);
+mapFace(2);
+mapFace(3);
+
