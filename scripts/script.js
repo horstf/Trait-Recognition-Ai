ce3071fd60ba7d6220dc59523fa7927f0437bf97
Horst Fellenberg <horst.fellenberg@tuwien.ac.at>
2021-03-21 12:40:35 +0100

    removed patches integration

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,47 @@
-const Patches = require('Patches');
 const Random = require('Random');
 const Reactive = require('Reactive');
-const Diagnostics = require('Diagnostics');
 const TouchGestures = require('TouchGestures');
+const Scene = require('Scene');
+const Diagnostics = require('Diagnostics')
+const FaceTracking = require('FaceTracking')
 
-const f1bool = Patches.getBooleanValue('f1boolval');
+const names = ['creep', 'stalker', 'loner', 'weirdo', 'sad', 'social media addict', 'insecure', 'jealous', 'self sabotaging', 'anger problem', 'anxious', 'narcissist', 'attention seeker', 'inhibited', 'cautious', 'skeptic', 'not ok', 'distant', 'misanthrope', 'naive', 'disillusioned',
+'hopeless'];
 
-const p1 = Patches.getPulseValue('Pulse1');
-
-const names = ['creep', 'stalker', 'loner', 'weirdo', 'sad', 'social media addict', 'insecure', 'jealous', 'self sabotaging', 'anger problem', 'anxious', 'narcissist', 'attention seeker', 'inhibited', 'cautious', 'skeptic'];
-
-let newDisp = function() {
-  const label = names[Math.floor(Random.random()*names.length)];
-  const pz = Math.round(Random.random()*10000)/100+'%';
-  Patches.setStringValue('newLabel1', Reactive.val(label));
-  Patches.setStringValue('newPz1', Reactive.val(pz));
+async function newDisp(label, confidence) {
+  label.text = Reactive.val(names[Math.floor(Random.random()*names.length)]);
+  confidence.text = Reactive.val(Math.round(Random.random()*10000)/100+'%');
+  Diagnostics.log("new trait: " + label.text.pinLastValue() + 
+    ", " + confidence.text.pinLastValue());
 }
 
-p1.subscribe(function() {
-  if (f1bool) {
-    newDisp();
-  }
-})
 
-TouchGestures.onTap().subscribe(newDisp);
+//map objects and make predictions for face
+async function mapFace(face) {
+  const canvas = await Scene.root.findFirst('face1');
+  const label = await Scene.root.findFirst('label1');
+  const confidence = await Scene.root.findFirst('confidence1')
+
+  //map face coordinates to canvas
+  canvas.transform.x = face.cameraTransform.x
+  canvas.transform.y = face.cameraTransform.y
+  canvas.transform.z = face.cameraTransform.z
+  canvas.visible = face.isTracked
+
+  //make new prediction when face visibility changes
+  
+  face.isTracked.monitor().subscribe(function() {
+    newDisp(label, confidence);
+  });
+
+  //make new prediction on tap
+  TouchGestures.onTap().subscribe(function() {
+    newDisp(label, confidence);
+  });
+
+  //make first prediction
+  newDisp(label, confidence);
+};
+
+//map first face
+mapFace(FaceTracking.face(0));
